328523bf4da811ac5e122af6c5a951441c953d88
gytterberg <gytterberg@gmail.com>
2021-06-26 12:05:09 -0400

    got basic image processing done

--- client/App.js
+++ client/App.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+// import ScriptLoader from 'react-script-loader-hoc';
 
 import Navbar from './components/Navbar';
 import Routes from './Routes';
 
 import Canvas from './components/Canvas';
 
-const App = () => {
+const App = ({ scriptsLoadedSuccessfully }) => {
   return (
     <div>
       <Canvas />
     </div>
   );
 };
+
+// export default ScriptLoader(
+//   'https://cdn.jsdelivr.net/gh/wallat/compiled-opencvjs/v4.2.0/opencv.js'
+// )(App);
 
 export default App;
--- client/components/Canvas.js
+++ client/components/Canvas.js
@@ -1,27 +1,30 @@
 import React, { useRef, useEffect, useState, useCallback } from 'react';
-// import localImage from '../../public/raw/ash-pikachu.jpg';
+import rgbquant from 'rgbquant';
+let cv = window.cv;
 
 const Canvas = (props) => {
+  const status = useScript(
+    'https://cdn.jsdelivr.net/gh/wallat/compiled-opencvjs/v4.2.0/opencv.js'
+  );
+  const [waitForFuckingCV, setWaitForFuckingCV] = useState(false);
+
   const canvasRef = useRef(null);
   const imgRef = useRef(null);
-  // const [canvas, setCanvas] = useState(null);
-  // // let img = useRef(null);
-  // const [ctx, setCtx] = useState(null);
 
-  const remoteImg =
-    'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dahlia-1508785047.jpg?crop=1.00xw:0.669xh;0,0.0136xh&resize=480:*';
-  //'https://secure.img1-fg.wfcdn.com/im/02238154/compr-r85/8470/84707680/pokemon-pikachu-wall-decal.jpg';
-  const localImg = '/raw/ash-pikachu.jpg';
-  // const imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-
-  const invert = (data) => {
-    for (let i = 0; i < data.length; i++) {
-      data[i] = 100;
-    }
-  };
+  const pika = '/raw/ash-pikachu.jpg';
+  const dahlia = '/raw/dahlia.jpg';
 
   /// set up image
   useEffect(() => {
+    console.log('In useeffect, status = ' + status);
+    cv = window.cv;
+    if (status !== 'ready' || typeof cv === 'undefined') {
+      return;
+    }
+    // if (cv) {
+    console.log('We got here');
+    console.log(cv);
+
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
 
@@ -31,53 +34,143 @@ const Canvas = (props) => {
 
     // console.log(localImage);
 
-    const drawImage = (image) => {
+    const quantizeImage = (image) => {
       // Set the canvas the same width and height of the image
 
       canvas.width = image.width;
       canvas.height = image.height;
 
-      ctx.filter = 'contrast(175%) blur(15px)';
+      ctx.filter = 'contrast(175%) blur(7px)';
 
-      ctx.drawImage(invert(image), 0, 0);
+      ctx.drawImage(image, 0, 0);
+
+      const quantizerOptions = {
+        colors: 16,
+      };
+      const quantizer = new rgbquant(quantizerOptions);
+      quantizer.sample(canvas);
+      let pal = quantizer.palette(false);
+      let out = quantizer.reduce(canvas);
+      let outImg = new ImageData(
+        new Uint8ClampedArray(out),
+        canvas.width,
+        canvas.height
+      );
+      ctx.putImageData(outImg, 0, 0);
+    };
+
+    const drawEdges = (canvas) => {
+      // let quantizedImgData = ctx.getImageData(
+      //   0,
+      //   0,
+      //   canvas.width,
+      //   canvas.height
+      // );
+
+      const source = cv.imread(canvas); // load the image from <img>
+      const dest = new cv.Mat();
+
+      // turn image grayscale for edge detection
+      cv.cvtColor(source, source, cv.COLOR_RGB2GRAY, 0);
+
+      // detect edges, keep playing with parameters
+      cv.Canny(source, dest, 50, 100, 3, false);
+
+      // invert image, turn lines black
+      cv.bitwise_not(dest, dest);
+
+      cv.imshow(canvas, dest); // display the output to canvas
+
+      source.delete(); // remember to free the memory
+      dest.delete();
+    };
+
+    const whiteToTransparent = () => {
+      // have canvas/image
     };
 
     img.onload = () => {
       // ctx.canvas.width = img.width;
       // ctx.canvas.height = img.height;
-      drawImage(img);
+      quantizeImage(img);
+      drawEdges(canvas);
     };
-  }, []);
+  }, [waitForFuckingCV]);
 
   return (
     <>
       <canvas width="10" height="10" ref={canvasRef} {...props} />
-      <img src={localImg} ref={imgRef} hidden={true} alt="img" />
+      <img src={dahlia} ref={imgRef} hidden={true} alt="img" />
+      <button
+        onClick={() => {
+          console.log('clicked');
+          setWaitForFuckingCV(true);
+        }}
+      >
+        Hopefully OpenCV is loaded ¯\_(ツ)_/¯
+      </button>
+      {/* <img src={pika} ref={imgRef} hidden={true} alt="img" /> */}
     </>
   );
 };
 
+// Hook from https://usehooks.com/useScript/
+function useScript(src) {
+  // Keep track of script status ("idle", "loading", "ready", "error")
+  const [status, setStatus] = useState(src ? 'loading' : 'idle');
+  useEffect(
+    () => {
+      // Allow falsy src value if waiting on other data needed for
+      // constructing the script URL passed to this hook.
+      if (!src) {
+        setStatus('idle');
+        return;
+      }
+      // Fetch existing script element by src
+      // It may have been added by another intance of this hook
+      let script = document.querySelector(`script[src="${src}"]`);
+      if (!script) {
+        // Create script
+        script = document.createElement('script');
+        script.src = src;
+        // script.async = true;
+        script.setAttribute('data-status', 'loading');
+        // Add script to document body
+        document.body.appendChild(script);
+        // Store status in attribute on script
+        // This can be read by other instances of this hook
+        const setAttributeFromEvent = (event) => {
+          script.setAttribute(
+            'data-status',
+            event.type === 'load' ? 'ready' : 'error'
+          );
+        };
+        script.addEventListener('load', setAttributeFromEvent);
+        script.addEventListener('error', setAttributeFromEvent);
+      } else {
+        // Grab existing script status from attribute and set to state.
+        setStatus(script.getAttribute('data-status'));
+      }
+      // Script event handler to update status in state
+      // Note: Even if the script already exists we still need to add
+      // event handlers to update the state for *this* hook instance.
+      const setStateFromEvent = (event) => {
+        setStatus(event.type === 'load' ? 'ready' : 'error');
+      };
+      // Add event listeners
+      script.addEventListener('load', setStateFromEvent);
+      script.addEventListener('error', setStateFromEvent);
+      // Remove event listeners on cleanup
+      return () => {
+        if (script) {
+          script.removeEventListener('load', setStateFromEvent);
+          script.removeEventListener('error', setStateFromEvent);
+        }
+      };
+    },
+    [src] // Only re-run effect if script src changes
+  );
+  return status;
+}
+
 export default Canvas;
-
-// const draw = (ctx, frameCount) => {
-//   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-//   ctx.fillStyle = '#000000';
-//   ctx.beginPath();
-//   ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
-//   ctx.fill();
-// };
-
-// useEffect(() => {
-// let frameCount = 0;
-// let animationFrameId;
-// //Our draw came here
-// const render = () => {
-//   frameCount++;
-//   draw(ctx, frameCount);
-//   animationFrameId = window.requestAnimationFrame(render);
-// };
-// render();
-// return () => {
-//   window.cancelAnimationFrame(animationFrameId);
-// };
-// }, []);
